bab3259251a3d58d3d79ae04c7a4c14bd7d09d29
Nikhil Reddy Purumandla <31058257+NikhilReddyPurumandla@users.noreply.github.com>
2017-12-07 17:58:34 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import {GithubComponent} from './components/github/github.component';
 import { StatusComponent } from './components/status/status.component';
 import { ChartsModule } from 'ng2-charts';
 import { RouterModule, Routes } from '@angular/router';
+import { OrganisationComponent } from './components/organisation/organisation.component';
+
 const routes: Routes = [
   { path: 'status', component: StatusComponent },
   { path: 'github', component: GithubComponent },
+  { path: 'organisation', component: OrganisationComponent },
   {
     path:'',
     redirectTo:'/status',
@@ -21,7 +24,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [ BrowserModule, HttpModule, FormsModule,ChartsModule, RouterModule.forRoot(routes)],
-  declarations: [ AppComponent, GithubComponent, StatusComponent ],
+  declarations: [ AppComponent, GithubComponent, StatusComponent, OrganisationComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
